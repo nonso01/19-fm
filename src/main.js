cf9a8f873744de6cfbeb66944427609213e0565c
nonso01 <chukwunonsomartin01@gmail.com>
2025-04-18 14:01:04 +0100

    fixes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 
-
-const log = console.log
+const log = console.log;
+const pickOne = (el) => document.querySelector(el);
+const pickAll = (els) => document.querySelectorAll(els);
