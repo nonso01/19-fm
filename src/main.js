13788fcd8c3f67213265cdabdb7eee279727965a
nonso01 <chukwunonsomartin01@gmail.com>
2025-04-23 17:45:38 +0100

    styles

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const log = console.log;
 const pickOne = (el) => document.querySelector(el);
 const pickAll = (els) => document.querySelectorAll(els);
 
+const swapBtns = pickAll('.div-swap p')
+const stacks = pickAll('.stack')
+
 // animate("img", {
 //     rotate: 180 *2 ,
 //     loop: true,
