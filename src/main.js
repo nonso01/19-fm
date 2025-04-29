d4e5b269d236b47794dec9a6aff9836355fd041e
nonso01 <chukwunonsomartin01@gmail.com>
2025-04-29 10:12:39 +0100

    script

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import "./style.css";
-import { animate } from "animejs";
+import { animate, createDraggable, utils, waapi, stagger } from "animejs";
 
 const log = console.log;
 const pickOne = (el) => document.querySelector(el);
 const pickAll = (els) => document.querySelectorAll(els);
 
-const swapBtns = pickAll('.div-swap p')
-const stacks = pickAll('.stack')
+const swapBtns = pickAll(".div-swap p");
+const stacks = pickAll(".stack");
 
-// animate("img", {
-//     rotate: 180 *2 ,
-//     loop: true,
-//     ease: 'inOutExpo',
-// });
+// for the extensions
+waapi.animate(".extension .add", {
+  translate: `0 -2rem`,
+  delay: stagger(100),
+  duration: 600,
+  loop: true,
+  alternate: true,
+  ease: "inOut(2)",
+});
+
+// createDraggable('.add', {
+//     container: '.extension'
+// })
