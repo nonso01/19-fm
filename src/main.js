3bd6fb16920bb7fd14a26fd1d311ef5a390f0d22
nonso01 <chukwunonsomartin01@gmail.com>
2025-05-07 12:11:47 +0100

    animations

--- src/main.js
+++ src/main.js
@@ -1,23 +1,47 @@
 import "./style.css";
-import { animate, createDraggable, utils, waapi, stagger } from "animejs";
+import {
+  animate,
+  createDraggable,
+  // utils,
+  waapi,
+  stagger,
+  onScroll,
+} from "animejs";
 
 const log = console.log;
+const ONESEC = 1e3;
 const pickOne = (el) => document.querySelector(el);
 const pickAll = (els) => document.querySelectorAll(els);
 
-const swapBtns = pickAll(".div-swap p");
-const stacks = pickAll(".stack");
-
-// for the extensions
-waapi.animate(".extension .add", {
-  translate: `0 -2rem`,
-  delay: stagger(100),
-  duration: 600,
-  loop: true,
-  alternate: true,
-  ease: "inOut(2)",
-});
+// const swapBtns = pickAll(".div-swap p");
+// const stacks = pickAll(".stack");
 
 // createDraggable('.add', {
 //     container: '.extension'
 // })
+
+(function pageAnimation() {
+  // blink the button dot
+  animate("nav button.red span", {
+    color: ["hsl(0, 94%, 66%)", "hsl(229, 31%, 21%)"],
+    duration: ONESEC,
+    alternate: true,
+    loop: true,
+  });
+
+  // drag the first illustration
+  createDraggable(".sb-image img", {
+    container: ".sec-sb",
+    snap: [0, 300],
+  });
+
+  // extension animation
+  waapi.animate(".extension .add", {
+    translate: `0 -2rem`,
+    delay: stagger(100),
+    duration: 600,
+    loop: true,
+    alternate: true,
+    ease: "inOut(2)",
+  });
+})();
